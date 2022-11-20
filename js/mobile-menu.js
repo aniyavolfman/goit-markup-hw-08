96437fc75e7fb0cc675e82f0d76bc2a08b3bfb60
Anna Volfman <aniya_07@ukr.net>
2022-11-20 12:53:26 +0200

    додає адаптив

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,12 +4,10 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
